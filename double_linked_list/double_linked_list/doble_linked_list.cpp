3429cf3fe45eda02d2664b821ce6f032c4be4b07
sasha_shvalev <sashashvalev64@gmail.com>
2016-10-20 19:48:14 +0300

    утечка памяти

--- double_linked_list/double_linked_list/doble_linked_list.cpp
+++ double_linked_list/double_linked_list/doble_linked_list.cpp
@@ -1,7 +1,17 @@
 #include "list.h"
+/**/
+#ifdef _DEBUG
+#include <crtdbg.h>
+#define _CRTDBG_MAP_ALLOC
+#endif
+/**/
 
 int main()
 {
+	/**
+	_CrtMemState _ms;
+	_CrtMemCheckpoint(&_ms);
+	/**/
 	DoubleLinkedList DLL;
 	for (int i = 0; i < 10; i++)
 	{
@@ -10,6 +20,11 @@ int main()
 	DLL.insert(3);
 	DLL.append(5);
 	DLL.printList();
+	/**/
+	_CrtDumpMemoryLeaks();
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+	/**/
 	_getch();
 	return 0;
 }
